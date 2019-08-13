875429cfc65767668e25c512f2d5995c98343b93
hreypar <hreypar@gmail.com>
2019-08-13 12:41:04 -0500

    update bowtie2 index path

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@
 BOWTIE2_THREADS="2"
 
 # path to genome index files 
-GENOME_INDEX_PATH="/opt/reference-genomes/Rattus_norvegicus/NCBI/Rnor_6.0/Sequence/Bowtie2Index"
+GENOME_INDEX_PATH="/opt/reference-genomes/Rattus_norvegicus/NCBI/Rnor_6.0/Sequence/Bowtie2Index/"
 
 # path to picard tools jar file
 PICARD_TOOLS_PATH="/opt/picard.jar"
