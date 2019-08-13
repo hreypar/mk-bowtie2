f784844c3cf0c2461d16674edde42c96e79c9b7f
hreypar <hreypar@gmail.com>
2019-08-13 11:57:35 -0500

    create env variables

---
+++ config.mk
@@ -0,0 +1,11 @@
+# Variables that should actually be environment vars.
+#
+# bowtie threads
+BOWTIE2_THREADS="2"
+
+# path to genome index files 
+GENOME_INDEX_PATH="/opt/reference-genomes/Rattus_norvegicus/NCBI/Rnor_6.0/Sequence/Bowtie2Index"
+
+# path to picard tools jar file
+PICARD_TOOLS_PATH="/opt/picard.jar"
+
